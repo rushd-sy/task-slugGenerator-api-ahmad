5c992d8594e4364de5732a7212b30b62b561b8e3
AhmadDabbagh <ahmadbesherdabbagh@gmail.com>
2026-05-13 17:57:20 +0300

    feat: implement iteration 2

--- SlugApi/SlugApi/Program.cs
+++ SlugApi/SlugApi/Program.cs
@@ -1,5 +1,8 @@
 
 using Asp.Versioning;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SlugApi.Exceptions;
 
 namespace SlugApi
 {
@@ -13,6 +16,8 @@ namespace SlugApi
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddOpenApi();
+            builder.Services.AddProblemDetails();
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
             builder.Services.AddApiVersioning(options =>
             {
                 options.DefaultApiVersion = new ApiVersion(1, 0);
@@ -20,7 +25,8 @@ namespace SlugApi
             });
             var app = builder.Build();
 
-
+            app.UseExceptionHandler();
+      
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -30,7 +36,6 @@ namespace SlugApi
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
